114e656cbb22a039b828c38b1b34ae04e35d4cbd
CanhHoang <nguyencanhhoangnh2019@gmail.com>
2022-03-10 10:23:04 +0700

    mainpage, footer, header

---
+++ mvc/views/pages/main.php
@@ -0,0 +1,42 @@
+<?php include './mvc/views/components/header.php'?>
+<div id="body">
+    <img src="public/images/homepage-banner.jpeg" alt="banner">
+    <div id="box">
+        <div>
+            <p class= "des-head">
+               Giao hàng toàn quốc
+            </p>    
+            <p class= "Describe">
+                Miễn phí vận chuyển với các đơn hàng trên 20.000.000 đ
+            </p>
+        </div>
+        <div>
+            <p class= "des-head">
+                Hỗ trợ online 24/7
+            </p>
+            <p class= "Describe">
+                Luôn hỗ trợ khách hàng tất cả các khung giờ trong tuần
+            </p>
+        </div>
+        <div>
+            <p class= "des-head">
+                Quà tặng hấp dẫn
+            </p>
+            <p class= "Describe">
+                Các quà tặng và voucher đi kèm với sản phẩm vô cùng hấp dẫn
+            </p>
+        </div>
+        <div>
+            <p class= "des-head">
+                Ưu đãi cho sinh viên
+            </p>
+            <p class= "Describe">
+                Giảm giá 20% cho tất cả mặt hàng cho sinh viên
+            </p>
+        </div>
+    </div>
+    <div id="link">
+        <button class="btn btn-primary"> Tới trang sản phẩm</button>
+    </div>
+</div>
+<?php include './mvc/views/components/footer.php'?>
